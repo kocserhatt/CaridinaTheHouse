f781f5116c41ac695c2fe950b0c880151b1cab8f
serhat koç <kocserhat671@gmail.com>
2024-07-26 21:23:39 +0300

    post kısmına bilgiler eklendi

--- src/component/cards.jsx
+++ src/component/cards.jsx
@@ -12,133 +12,121 @@ function BasicExample() {
         <h1>POSTLAR</h1>
     <Row style={{gap:"20px", justifyContent:"center"}}>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img4.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Tür Çeşitliliği</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karidesler, dünyada yaklaşık 2.000 farklı türü bulunan kabuklu deniz canlılarıdır.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img5.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Habitat</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Genellikle tuzlu sularda yaşasalar da, bazı türler tatlı su ortamlarında da bulunabilir.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img6.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Beslenme</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karidesler omnivor olup, plankton, küçük balıklar ve organik maddelerle beslenirler.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img7.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Boyut</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karidesler, türlerine bağlı olarak birkaç santimetreden 30 santimetreye kadar değişen boyutlarda olabilirler.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img8.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Renk Değişimi</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Bazı karides türleri çevrelerine uyum sağlamak için renk değiştirme yeteneğine sahiptir.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img9.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Üreme</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Dişi karidesler, yumurtalarını suya bırakır ve erkek karidesler tarafından döllenirler.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img10.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Ekonomik Değer</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karidesler, dünya çapında önemli bir ticari değere sahip olup, balıkçılık ve yetiştiricilikte yaygın olarak bulunur.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img11.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Sindirim Sistemi</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karideslerin sindirim sistemi oldukça basittir ve genellikle karınlarının alt kısmında bulunur.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img12.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Savunma Mekanizması</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Bazı karides türleri, düşmanlarından korunmak için hızlı yüzme yeteneklerini kullanır.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img13.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>İskelet Yapısı</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karideslerin dış iskeletleri sert bir kalsiyum karbonat kabuğundan oluşur ve büyüdükçe kabuklarını değiştirirler.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img14.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Temizlikçi Karidesler</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Bazı karides türleri, diğer balıkların üzerinde biriken parazitleri temizleyerek simbiyotik ilişkiler kurar.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/img1.jpg" />
+      <Card.Img variant="top" src="/img15.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Sosyal Davranış</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Karidesler genellikle sürü halinde yaşar ve sosyal davranışlar sergileyebilirler.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
