abc925d10ae77110fdfa05aed109194a5450e447
serhat koç <kocserhat671@gmail.com>
2024-07-26 22:18:16 +0300

    home düzenleme

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -11,7 +11,6 @@ import {
 export default function Footer() {
   return (
     <>
-    <div id='iletisim'></div>
     <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
       <MDBContainer className='pt-4'>
         <section className='mb-4'>
@@ -92,7 +91,7 @@ export default function Footer() {
       <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:
         <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+          Kocserhatt
         </a>
       </div>
     </MDBFooter>
